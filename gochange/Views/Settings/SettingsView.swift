1a18c045673aefd2421ec68666791c6228e04617
Toqi Tahamid <2114722+toqitahamid@users.noreply.github.com>
2025-11-25 04:03:54 -0600

    Add FSCalendar package dependency and update settings and workout views to include necessary imports

--- gochange/Views/Settings/SettingsView.swift
+++ gochange/Views/Settings/SettingsView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import SwiftData
+import UniformTypeIdentifiers
 
 struct SettingsView: View {
     @Environment(\.modelContext) private var modelContext
--- gochange/Views/Workout/ActiveWorkoutView.swift
+++ gochange/Views/Workout/ActiveWorkoutView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import SwiftData
+import Combine
 
 struct ActiveWorkoutView: View {
     @Environment(\.modelContext) private var modelContext
